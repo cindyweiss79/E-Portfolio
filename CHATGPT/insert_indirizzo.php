aabbe9d3b77c6729b87f825f5918ae0c54340fc6
cindyweiss79 <cindy.weiss@outlook.it>
2025-05-03 17:42:58 +0200

    Update insert_indirizzo.php

--- CHATGPT/insert_indirizzo.php
+++ CHATGPT/insert_indirizzo.php
@@ -1,19 +1,1 @@
-<?php
-require 'config.php';
 
-$sql = "INSERT INTO Indirizzo (ID_Utente, Paese_Indirizzo, Provincia_Indirizzo, Comune_Indirizzo, CAP_Indirizzo, VIA_Indirizzo, Numero_civico)
-        VALUES (:id_utente, :paese, :provincia, :comune, :cap, :via, :civico)";
-
-$stmt = $pdo->prepare($sql);
-$stmt->execute([
-    ':id_utente' => 1,
-    ':paese' => 'Italia',
-    ':provincia' => 'BZ',
-    ':comune' => 'Innichen',
-    ':cap' => 39038,
-    ':via' => 'J. Cornet',
-    ':civico' => '8/c'
-]);
-
-echo "Indirizzo inserito!";
-?>
