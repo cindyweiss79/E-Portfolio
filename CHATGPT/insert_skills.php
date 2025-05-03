60124aec61c0c9ee28658cc9d1964532b4943215
cindyweiss79 <cindy.weiss@outlook.it>
2025-05-03 17:42:30 +0200

    Update insert_skills.php

--- CHATGPT/insert_skills.php
+++ CHATGPT/insert_skills.php
@@ -1,16 +1,1 @@
-<?php
-require 'config.php';
 
-$sql = "INSERT INTO Skill (ID_Utente, Nome_Skill, Percentuale, Link_Media)
-        VALUES (:id_utente, :skill, :percentuale, :media)";
-
-$stmt = $pdo->prepare($sql);
-$stmt->execute([
-    ':id_utente' => 1,
-    ':skill' => 'PHP',
-    ':percentuale' => 85,
-    ':media' => 'https://linkallmedia.com'
-]);
-
-echo "Skill inserita!";
-?>
