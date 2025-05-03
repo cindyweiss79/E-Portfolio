7f32e91077a5716fb08c17ef18e1b38e66d06e8c
cindyweiss79 <cindy.weiss@outlook.it>
2025-05-03 17:42:44 +0200

    Update insert_progetto.php

--- CHATGPT/insert_progetto.php
+++ CHATGPT/insert_progetto.php
@@ -1,16 +1,1 @@
-<?php
-require 'config.php';
 
-$sql = "INSERT INTO Progetto (ID_Utente, Titolo_Progetto, Descrizione, link_progetto)
-        VALUES (:id_utente, :titolo, :descrizione, :link)";
-
-$stmt = $pdo->prepare($sql);
-$stmt->execute([
-    ':id_utente' => 1,
-    ':titolo' => 'Project 01',
-    ':descrizione' => 'Descrizione del progetto',
-    ':link' => 'https://linkprogetto.com'
-]);
-
-echo "Progetto inserito!";
-?>
